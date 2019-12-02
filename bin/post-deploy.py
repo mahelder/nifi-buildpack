96f142545a244b85954097ee2c71e86c8461a58c
Mahelder Melo <mahelder.melo@luizalabs.com>
2019-12-02 15:25:48 -0300

    feat: comenta tudo

--- bin/post-deploy.py
+++ bin/post-deploy.py
@@ -9,46 +9,44 @@ from xml.etree import ElementTree
 
 os.system('sh nifi/bin/nifi.sh start')
 
-print('ligando nifi')
-time.sleep(30)
+# print('ligando nifi')
+# time.sleep(30)
 
-base_url = 'nifi-sample.nifi-sample/nifi'
-base_url_api = 'nifi-sample.nifi-sample/nifi-api'
-response = requests.get(base_url)
+# base_url = 'nifi-sample.nifi-sample/nifi'
+# base_url_api = 'nifi-sample.nifi-sample/nifi-api'
+# response = requests.get(base_url)
 
-while response.status_code != 200
-    time.sleep(10)
-    response = requests.get(base_url)
+# while response.status_code != 200
+#     time.sleep(10)
+#     response = requests.get(base_url)
 
-# get process group id
-response = requests.get(base_url_api + '/flow/process-groups/root/status?recursive=true')
-process_group_id = response.json()['processGroupStatus']['id']
-
-
-# upload template in process group
-files = {'template': open('template-import.xml')}
-response = requests.post(base_url_api + 'process-groups/{0}/templates/upload'.format(process_group_id), files=files)
-tree = ElementTree.fromstring(response.content)
-template_id = tree[0][2].text
+# # get process group id
+# response = requests.get(base_url_api + '/flow/process-groups/root/status?recursive=true')
+# process_group_id = response.json()['processGroupStatus']['id']
 
 
-# activate template in process group
-data = {
-    "originX": 2.0,
-    "originY": 3.0,
-    "templateId": template_id
-}
-response = requests.post(base_url_api + 'process-groups/{0}/template-instance'.format(process_group_id), data=data)
+# # upload template in process group
+# files = {'template': open('template-import.xml')}
+# response = requests.post(base_url_api + 'process-groups/{0}/templates/upload'.format(process_group_id), files=files)
+# tree = ElementTree.fromstring(response.content)
+# template_id = tree[0][2].text
 
 
-# start process group
-data = {
-    "id": process_group_id,
-    "state": "RUNNING"
-}
-response = requests.put(base_url_api + 'flow/process-groups/{0}'.format(process_group_id), data=data)
+# # activate template in process group
+# data = {
+#     "originX": 2.0,
+#     "originY": 3.0,
+#     "templateId": template_id
+# }
+# response = requests.post(base_url_api + 'process-groups/{0}/template-instance'.format(process_group_id), data=data)
 
 
+# # start process group
+# data = {
+#     "id": process_group_id,
+#     "state": "RUNNING"
+# }
+# response = requests.put(base_url_api + 'flow/process-groups/{0}'.format(process_group_id), data=data)
 
-while True:
-    time.sleep(30)
+# while True:
+    # time.sleep(30)
