e7dafd6b08a44cfd31b2f591331e97026412e522
Mahelder Melo <mahelder.melo@luizalabs.com>
2019-12-02 16:43:51 -0300

    feat: teste com start.sh

---
+++ bin/start.sh
@@ -0,0 +1,10 @@
+NIFI_HOME=/app/nifi
+
+tail -F "${NIFI_HOME}/logs/nifi-app.log" &
+"${NIFI_HOME}/bin/nifi.sh" run &
+nifi_pid="$!"
+
+trap "echo Received trapped signal, beginning shutdown...;" KILL TERM HUP INT EXIT;
+
+echo NiFi running with PID ${nifi_pid}.
+wait ${nifi_pid}
